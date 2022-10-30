45123396b98a6f690d7a12f5067843a748d836c4
iamded <dedetmasri@gmail.com>
2022-10-30 22:11:03 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg install python bash git -y
+pkg install python2 -y
+pip install requests colorama bs4
+pip2 install colorama bs4 requests
+pkg update -y
+pkg upgrade -y
+echo "Package alredy install - running tools"
+clear
+sleep 3
+python runcam.py
